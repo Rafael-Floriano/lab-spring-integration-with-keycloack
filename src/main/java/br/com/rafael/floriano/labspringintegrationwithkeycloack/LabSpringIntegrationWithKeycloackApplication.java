9c065d9a7a37a0d4663ea3709da1efa26860c32d
Rafael Floriano <rafael.floriano@gmail.com>
2024-03-11 19:55:10 -0300

    FEAT: create a new project to learn about keycloack

---
+++ src/main/java/br/com/rafael/floriano/labspringintegrationwithkeycloack/LabSpringIntegrationWithKeycloackApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.labspringintegrationwithkeycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringIntegrationWithKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringIntegrationWithKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafael/floriano/labspringintegrationwithkeycloack/LabSpringIntegrationWithKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.labspringintegrationwithkeycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringIntegrationWithKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
